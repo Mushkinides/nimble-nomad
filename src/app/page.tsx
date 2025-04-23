51c1b8a29284ac3e26104219c70ff2d55c6e0158
Tal.M <t.m.mushkin@gmail.com>
2025-04-23 15:59:48 +0300

    style: :art: Added sign-in page  & wireframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
+  const fullUser = null;
+  // const fullUser = { id: "", name: "Tal", role: "user" };
+
   return (
     <div className="bg-background text-foreground">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Nimble Nomad
       </h1>
-      <Button>Create new character</Button>
+      {fullUser == null ? (
+        <div className="flex gap-4">
+          <Button asChild>
+            <Link href="/sign-in">Sign In</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </div>
+      ) : (
+        // <Card className="max-w-[500px] mt-4"></Card>
+        <Button>Create new character</Button>
+      )}
     </div>
   );
 }
